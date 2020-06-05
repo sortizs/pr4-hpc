4e70a05ec317342b49ffc60d88c90138d9394789
Stefania Zapata Osorio <szapat25@eafit.edu.co>
2020-06-05 15:12:09 -0700

    añadiendo mpi

---
+++ mpi/mpi_acc_ciudad.py
@@ -0,0 +1,16 @@
+from mpi4py import MPI
+import pandas as pd
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+#se lee el archivo
+df = pd.read_csv('../dataset/datos.csv', delimiter = ';')
+df.head()
+
+#se filtran los datos por ciudad
+dfMunicipio = df.filter(items = [df.columns[1]])
+
+#se agrupan los datos
+dfGrouped = dfMunicipio.groupby([dfMunicipio.columns[0]]).size()
