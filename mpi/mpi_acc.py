74a3bc17ddc9ad217022feb9c868c7ffd876194b
Stefania Zapata Osorio <szapat25@eafit.edu.co>
2020-06-05 18:58:36 -0700

    más cambios mpi

--- mpi/mpi_acc.py
+++ mpi/mpi_acc.py
@@ -11,25 +11,27 @@ df.head()
 def reduce(num):
     dfR = df.filter(items = [df.columns[num]])
     dfGrouped = dfR.groupby([dfR.columns[0]]).size()
-    
+    print(dfGrouped)
+    print("#####################################")
+
 if rank == 0:
     comm.send(1, dest = 1) # ciudad
     comm.send(5, dest = 2) # clase
     comm.send(4, dest = 3) # dia
     comm.send(7, dest = 4) # gravedad
+
 if rank == 1:
     num = comm.recv(source = 0)
     reduce(num);
-    print("Finaliza: ciudad")
+
 if rank == 2:
     num = comm.recv(source = 0)
     reduce(num)
-    print("Finaliza: clase")
+
 if rank == 3:
     num = comm.recv(source = 0)
     reduce(num)
-    print("Finaliza: dia")
+
 if rank == 4:
     num = comm.recv(source = 0)
     reduce(num)
-    print("Finaliza: gravedad")
